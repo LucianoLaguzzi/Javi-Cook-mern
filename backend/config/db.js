8a3887c1aca2fc895c7ddd5c323ab6fc8412d8b5
LucianoLaguzzi <luciano.laguzzi@gmail.com>
2024-11-08 16:48:09 -0300

    Probando render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 
 const conectarDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/javicook');
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/javicook';
+
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
     console.log('Conectado a MongoDB');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error.message);
