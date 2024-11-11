ec78dada4eeb3ee9741763cedc603d9f1e8ae60c
LucianoLaguzzi <luciano.laguzzi@gmail.com>
2024-11-11 17:04:55 -0300

    Probando render16

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://javicook-mern-front.onrender.com', // Dominio de tu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Conexión a MongoDB
