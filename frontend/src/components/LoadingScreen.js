d65a88948eef18af01a472bd274874b83c876674
LucianoLaguzzi <luciano.laguzzi@gmail.com>
2024-11-14 16:55:32 -0300

    Espera en login10

--- frontend/src/components/LoadingScreen.js
+++ frontend/src/components/LoadingScreen.js
@@ -8,7 +8,7 @@ const LoadingScreen = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             navigate('/login');
-        }, 4000);
+        }, 90000);
 
         return () => clearTimeout(timer);
     }, [navigate]);
