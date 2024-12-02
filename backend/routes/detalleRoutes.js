46ab8d8d5cd36353f8faaa13285281de7a021f4e
LucianoLaguzzi <luciano.laguzzi@gmail.com>
2024-12-02 19:45:20 -0300

    Prueba respuestas comentarios21

--- backend/routes/detalleRoutes.js
+++ backend/routes/detalleRoutes.js
@@ -8,18 +8,14 @@ const router = express.Router();
 router.get('/:id', async (req, res) => {
     try {
         const receta = await Receta.findById(req.params.id)
-            .populate('usuario')  // Agrega el usuario de la receta
+            .populate('usuario') // Población para el usuario de la receta
             .populate({
                 path: 'comentarios',
                 populate: [
-                    { path: 'usuario', select: 'nombre imagenPerfil' },  // Usuario del comentario
-                    { path: 'parentComment', select: 'comentario' },  // Comentario original (para respuestas)
-                    {
-                        path: 'respuestas',  // Respuestas del comentario
-                        populate: [
-                            { path: 'usuario', select: 'nombre imagenPerfil' },  // Usuario de la respuesta
-                        ]
-                    }
+                    { path: 'usuario', select: 'nombre imagenPerfil' },
+                    { path: 'parentComment', select: 'comentario' },
+                    // Agregar esta línea para las respuestas de cada comentario
+                    { path: 'respuestas', populate: { path: 'usuario', select: 'nombre imagenPerfil' } }
                 ]
             });
 
@@ -27,10 +23,9 @@ router.get('/:id', async (req, res) => {
             return res.status(404).json({ mensaje: 'Receta no encontrada' });
         }
 
-        res.json(receta);  // Devuelve la receta con comentarios y respuestas
+        res.json(receta);
     } catch (error) {
         res.status(500).json({ mensaje: 'Error al cargar la receta', error });
     }
 });
-
 export default router;
