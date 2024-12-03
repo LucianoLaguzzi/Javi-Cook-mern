ca8ee1e35c3f8d841b3583bf373cd37974958762
LucianoLaguzzi <luciano.laguzzi@gmail.com>
2024-12-03 12:39:08 -0300

    Mejoras respuestas7

--- backend/routes/detalleRoutes.js
+++ backend/routes/detalleRoutes.js
@@ -5,15 +5,17 @@ import Receta from '../models/Receta.js';
 const router = express.Router();
 
 // Ruta para obtener receta por ID
+// Ruta para obtener receta por ID
 router.get('/:id', async (req, res) => {
     try {
+        // Verificar si la receta existe
         const receta = await Receta.findById(req.params.id)
             .populate('usuario') // Popula el usuario que creó la receta
             .populate({ 
                 path: 'comentarios', 
                 populate: { 
                     path: 'usuario', // Popula el usuario de cada comentario
-                    select: 'nombre imagenPerfil' // Puedes seleccionar los campos que quieres del usuario
+                    select: 'nombre imagenPerfil' // Campos específicos
                 },
                 options: { sort: { 'fecha': 1 } } // Ordenar los comentarios por fecha
             });
@@ -22,10 +24,10 @@ router.get('/:id', async (req, res) => {
             return res.status(404).json({ mensaje: 'Receta no encontrada' });
         }
 
-        // Cargar respuestas a comentarios, si existen
+        // Cargar respuestas a los comentarios, si existen
         const comentariosConRespuestas = await Comentario.find({
             receta: receta._id,
-            parentCommentId: { $ne: null } // Solo las respuestas (excluyendo comentarios principales)
+            parentCommentId: { $ne: null } // Solo las respuestas, no comentarios principales
         }).populate('usuario', 'nombre imagenPerfil');
 
         // Agregar las respuestas a sus comentarios correspondientes
@@ -33,8 +35,10 @@ router.get('/:id', async (req, res) => {
             comentario.respuestas = comentariosConRespuestas.filter(respuesta => respuesta.parentCommentId.toString() === comentario._id.toString());
         });
 
+        // Devolver receta con comentarios y respuestas
         res.json(receta);
     } catch (error) {
+        console.error('Error al cargar la receta', error); // Log de error para depuración
         res.status(500).json({ mensaje: 'Error al cargar la receta', error });
     }
 });
